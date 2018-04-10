f9098fa1ac8dccaed865d769d7f0848a4142e7de
Marlon E. Cobos <manubio13@gmail.com>
2018-04-09 19:15:52 -0500

    bug fixed

--- om_rate.R
+++ om_rate.R
@@ -3,6 +3,6 @@ ku.enm.omrat <- function(model, threshold, occ.tra, occ.test) {
   suit_val_eval <- na.omit(extract(model, occ.test))
   val <- ceiling(length(occ.tra[,1]) * threshold / 100) + 1
   omi_val_suit <- sort(suit_val_cal)[val]
-  om_rate <- as.numeric(length(suit_val_eval[suit_val_eval < omi.val.suit])/length(suit_val_eval))
+  om_rate <- as.numeric(length(suit_val_eval[suit_val_eval < omi_val_suit])/length(suit_val_eval))
   return(om_rate)
 }
