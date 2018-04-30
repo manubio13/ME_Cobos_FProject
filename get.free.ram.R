4c326e46569d1aabf7e7b74b93f210cbaa0d0b81
Marlon E. Cobos <manubio13@gmail.com>
2018-04-30 09:48:35 -0500

    Functions improved last

--- get.free.ram.R
+++ get.free.ram.R
@@ -1,3 +1,17 @@
+#' Get amount of RAM not occupied at the moment
+#'
+#' @description get.free.ream calculates the amount of RAM memory that is
+#' not being occupied at the moment.
+#'
+#' @return The amount of free RAM at the moment as a numeric value.
+#'
+#' @details At the moment only works for windows. For its use in the
+#' \code{\link{ku.enm.cal}} and \code{\link{ku.enm.mod}} functions for
+#' in other operative systems returns a value of 4 GB.
+#'
+#' @examples
+#' free_ram <- get.free.ram()
+
 get.free.ram <- function(){
   if(Sys.info()[["sysname"]] == "Windows"){
     x <- system2("wmic", args =  "OS get FreePhysicalMemory /Value", stdout = TRUE)
--- html.eval.R
+++ html.eval.R
@@ -1,10 +1,23 @@
-html.eval <- function(path, file.name) {
-  
-  if(!require(rmarkdown)){
-    install.packages("rmarkdown")
-    library(rmarkdown)
-  }
-  
+#' Creation of model calibration and selection html result
+#'
+#' @description html.eval creates an html file that summarizes all outputs from
+#' the model calibration and evaluation results.
+#'
+#' @param path directory in wich the html file will be written, default = current directory.
+#' @param file.name (character) name of the html file.
+#'
+#' @return An html file summarizing all the model calibration and selection results.
+#'
+#' @details This function is used along with the \code{\link{ku.enm.ceval}} function.
+#'
+#' @examples
+#' path <- "Evaluation_results"
+#' name <- "evaluation_results"
+#'
+#' html_file <- html.eval(path = path, file.name = name)
+
+html.eval <- function(path = getwd(), file.name) {
+
   sink(paste(path, paste(file.name, ".Rmd", sep = ""), sep = "/"))
   cat(
 "---
@@ -14,15 +27,15 @@ output:
       toc: true
       toc_depth: 4
 ---
-      
+
 \```{r setup, include=FALSE}
 knitr::opts_chunk$set(echo = TRUE)
 \```
 
 <br>
-      
-### Breif description of the models evaluation and selection process
-      
+
+### Brief description of the model evaluation and selection process
+
 \```{r, echo=FALSE}
 st4 <- read.csv(\"evaluation_results.csv\")
 sett <- as.character(st4[,1])
@@ -44,55 +57,57 @@ f.clas1 <- paste(unique(f.clas), collapse = \", \")
 var.di1 <- paste(unique(var.di), collapse = \", \")
 par <- rbind(rm1, f.clas1, var.di1)
 \```
-      
-This is the final report of the ku.enm.eval function implemented in the ku.enm R package. 
 
-A total of \`r length(st4[,1])\` calibration models with parameters resulted from the combination of \`r length(unique(rm))\` regularization multipliers, \`r length(unique(f.clas))\` feature classes, and \`r length(unique(var.di))\` distinct sets of environmental variables, have been evaluated. Models peformance was evaluated based on statistical significance (Partial_ROC), omission rates, and the Akaike information criterion corrected for small sample sizes (AICc).
-      
+This is the final report of the ku.enm.eval function implemented in the ku.enm R package.
+
+A total of \`r length(st4[,1])\` candidate models with parameters reflecting all combinations of \`r length(unique(rm))\` regularization multiplier settings, \`r length(unique(f.clas))\` feature classes combinations, and \`r length(unique(var.di))\` distinct sets of environmental variables, have been evaluated. Model peformance was evaluated based on statistical significance (Partial_ROC), omission rates, and the Akaike information criterion corrected for small sample sizes (AICc).
+
 \```{r par, echo=FALSE}
 colnames(par) <- \"Parameters\"
 row.names(par) <- c(\"Regularization multipliers\", \"Feature classes\", \"Sets of predictors\")
-knitr::kable(par, digits=c(0,0), row.names = TRUE, caption = \"Table 1. Parameters of the evaluated models.\")
+knitr::kable(par, digits=c(0,0), row.names = TRUE, caption = \"Table 1. Parameters of the candidate models.\")
 \```
-      
+
 <br>
 All the results presented below can be found in the evaluation output folder for further analyses.
 
 <br>
 <br>
-      
-### Models evaluation statistics
 
-In the following table you are going to find information about how many models meet the four criteria of selection that this function uses.
-      
+### Model evaluation statistics
+
+In the following table is information about how many models met the four criteria of selection that this function uses.
+
 \```{r, echo=FALSE}
-st <- read.csv(\"evaluation_stats.csv\")
+frow <- data.frame(\"Total candidate models\", length(st4[,1]))
+s <- read.csv(\"evaluation_stats.csv\")
+st <- rbind(frow, s)
 colnames(st) <- c(\"Criteria\",	\"Number_of_models\")
-knitr::kable(st, digits=c(0,0), caption = \"Table 2. General statistics of models that meet distinct criteria.\")
+knitr::kable(st, digits=c(0,0), caption = \"Table 2. General statistics of models that met distinct criteria.\")
 \```
-      
+
 <br>
 <br>
 
-### Best models according to pre-defined criteria
-      
+### Best models according to user-defined criteria
+
 The following table contains the best models selected by your pre-defined criteria.
 
-Notice that if the selection criterion was, models with the best Omission rates and among them those with lower AICc values, the delta_AICc values were recalculated among the new candidate models.
+Notice that if the selection criterion was \"OR_AICc\", models below the omission rate and among them those with lower AICc values, the delta AICc values were recalculated only among models below the omission rate (*E*).
 
 \```{r, echo=FALSE}
 best <- list.files(pattern = \"best\")
 st1 <- read.csv(best)
-colnames(st1) <- c(\"Model\",	\"Mean_AUC_ratio\",	\"Partial_ROC\", \"Ommission_rate_5%\", \"AICc\",	\"delta_AICc\",	\"W_AICc\",	\"num_parameters\")
+colnames(st1) <- c(\"Model\",	\"Mean_AUC_ratio\",	\"Partial_ROC\", \"gsub(\"\\.\", \"%\", colnames(st1)[4])\", \"AICc\",	\"delta_AICc\",	\"W_AICc\",	\"num_parameters\")
 knitr::kable(st1, digits=c(0,3,3,3,3,3,3,0), caption = \"Table 3. Performance statistics of the best models selected based on the pre-defined critera.\")
 \```
 
 <br>
 <br>
 
-### Models preformance plot
+### Model performance plot
 
-The figure below shows the position of your selected models in the distribution of all the calibration models according to omission rates and AICc values.
+The figure below shows the position of your selected models in the distribution of all candidate models in terms of statistical significance, omission rates, and AICc values.
 
 ![Figure 1. Distribution of all, non-statistically significant, and selected models according to their omission rates and AICc values.](evaluation_figure.png){width=60%}
 
@@ -101,19 +116,16 @@ The figure below shows the position of your selected models in the distribution 
 
 ### Performance evaluation statistics for all models
 
-Following you will find the performance statistics for all the calibration models.
+Following you will find the performance statistics for all candidate models.
 
 \```{r, echo=FALSE}
 st4 <- read.csv(\"evaluation_results.csv\")
-colnames(st4) <-  c(\"Model\",	\"Mean_AUC_ratio\",	\"Partial_ROC\", \"Ommission_rate_5%\", \"AICc\",	\"delta_AICc\",	\"W_AICc\",	\"num_parameters\")
-knitr::kable(st4, digits=c(0,3,3,3,3,3,3,0), caption = \"Table 4. Performance statistics of all the calibration models.\")
+colnames(st4) <-  c(\"Model\",	\"Mean_AUC_ratio\",	\"Partial_ROC\", \"gsub(\"\\.\", \"%\", colnames(st4)[4])\", \"AICc\",	\"delta_AICc\",	\"W_AICc\",	\"num_parameters\")
+knitr::kable(st4, digits=c(0,3,3,3,3,3,3,0), caption = \"Table 4. Performance statistics for all of the candidate models.\")
 \```"
       )
   sink()
   render(paste(path, paste(file.name, ".Rmd", sep = ""), sep = "/"), "html_document", quiet = TRUE)
   unlink(paste(path, paste(file.name, ".Rmd", sep = ""), sep = "/"))
-  
+
 }
-  
-  
-  
